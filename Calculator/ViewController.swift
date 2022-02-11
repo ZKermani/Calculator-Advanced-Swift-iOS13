53636422e3081010c9076672bd3fb4c2d2c65bd8
Zahra Kermani <zahrapkermani@gmail.com>
2022-02-11 12:39:22 +0100

    Display pressed number

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,20 +11,40 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
-    
+    private var didFinishTyping: Bool = true
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert displayLabel text to double")
+            }
+            return number
+        }
+        
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        didFinishTyping = true
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        if didFinishTyping {
+            displayLabel.text = sender.currentTitle
+            didFinishTyping = false
+        } else {
+            guard let num = sender.currentTitle else {
+                fatalError("num button has no title!")
+            }
+            displayLabel.text = displayLabel.text! + num
+        }
     }
 
 }
