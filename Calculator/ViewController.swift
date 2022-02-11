7764763c02c13b6253f82acee555fe73dd52db4e
Zahra Kermani <zahrapkermani@gmail.com>
2022-02-11 12:48:51 +0100

    Prevent multiple . in one number

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -38,10 +38,17 @@ class ViewController: UIViewController {
         //What should happen when a number is entered into the keypad
         if didFinishTyping {
             displayLabel.text = sender.currentTitle
+            print(displayValue)
             didFinishTyping = false
         } else {
             guard let num = sender.currentTitle else {
                 fatalError("num button has no title!")
+            }
+            if num == "." { // TODO: Still does not solve the .. issue
+                let isInt = floor(displayValue) == displayValue
+                if !isInt {
+                    return
+                }
             }
             displayLabel.text = displayLabel.text! + num
         }
